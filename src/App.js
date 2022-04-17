c626bf265e040be32b5bd2fef876ecc4a8e20bd5
Ayushi Keshri <ayushi.keshri@accenture.com>
2022-04-17 14:53:14 +0530

    add functionality to each square click

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import { Square } from './Square';
 import { useState } from 'react';
 
-const initialArr = ['X', 'O', '', '', '', '', '', '', '']
+const initialArr = ['', '', '', '', '', '', '', '', '']
 function App() {
   const [initialState, updateInitialState] = useState(initialArr);
+  const [isXmove, updateXMove] = useState(true) //initial value to be X
   const clearBoard = () => {
-    console.log('here')
     updateInitialState(initialArr);
+  }
+  const updateMoves = (index) => {
+    const arr = initialState;
+    arr[index] = isXmove ? 'X' : 'O'
+    updateInitialState(arr);
+    updateXMove(!isXmove);
   }
   return (
     <div className="App">
@@ -17,19 +23,19 @@ function App() {
         </p>
         <div className='Board'>
           <div style={{ display: 'flex' }}>
-            <Square border='bottom-right' val={initialState[0]} />
-            <Square border='bottom-right' val={initialState[1]} />
-            <Square border='bottom' val={initialState[2]} />
+            <Square border='bottom-right' index={0} val={initialState[0]} updateMoves={updateMoves} />
+            <Square border='bottom-right' index={1} val={initialState[1]} updateMoves={updateMoves} />
+            <Square border='bottom' index={2} val={initialState[2]} updateMoves={updateMoves} />
           </div>
-          <div style={{ display: 'flex' }} val={initialState[3]}>
-            <Square border='bottom-right' val={initialState[4]} />
-            <Square border='bottom-right' val={initialState[5]} />
-            <Square border='bottom' />
+          <div style={{ display: 'flex' }}  >
+            <Square border='bottom-right' index={3} val={initialState[3]} updateMoves={updateMoves} />
+            <Square border='bottom-right' index={4} val={initialState[4]} updateMoves={updateMoves} />
+            <Square border='bottom' index={5} val={initialState[5]} updateMoves={updateMoves} />
           </div>
-          <div style={{ display: 'flex' }} val={initialState[6]}>
-            <Square border='right' val={initialState[7]} />
-            <Square border='right' val={initialState[8]} />
-            <Square />
+          <div style={{ display: 'flex' }} >
+            <Square border='right' index={6} val={initialState[6]} updateMoves={updateMoves} />
+            <Square border='right' index={7} val={initialState[7]} updateMoves={updateMoves} />
+            <Square index={8} val={initialState[8]} updateMoves={updateMoves} />
           </div>
         </div>
         <button className='btn' onClick={() => clearBoard()}> Clear </button>
--- src/Square.js
+++ src/Square.js
@@ -2,7 +2,7 @@ import './App.css';
 
 export function Square(props) {
     return (
-        <div className={`Square ${props.border}`}>
+        <div className={`Square ${props.border}`} onClick={() => props.updateMoves(props.index)}>
             <div className={`Value ${props.val}`}>{props.val}</div>
         </div>)
 }
