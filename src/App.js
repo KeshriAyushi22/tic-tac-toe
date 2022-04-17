30f918a49b8152e2a15dada289ae6034e25fdb2e
Ayushi Keshri <ayushi.keshri@accenture.com>
2022-04-17 11:33:13 +0530

    board and grid sqr. setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { Square } from './Square';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tic-Tac-Toe
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='Board'>
+          <div style={{ display: 'flex' }}>
+            <Square />
+            <Square />
+            <Square />
+          </div>
+          <div style={{ display: 'flex' }}>
+            <Square />
+            <Square />
+            <Square />
+          </div>
+          <div style={{ display: 'flex' }}>
+            <Square />
+            <Square />
+            <Square />
+          </div>
+        </div>
       </header>
     </div>
   );
---
+++ src/Square.js
@@ -0,0 +1,8 @@
+import './App.css';
+
+export function Square() {
+    return (
+        <div className='Square'>
+            <div className='Value'>X</div>
+        </div>)
+}
